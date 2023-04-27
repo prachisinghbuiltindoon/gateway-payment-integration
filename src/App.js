faf87271fdc40042d590cd7c914c96ad46cc7f04
prachi singh <prachisingh@builtindoon.com>
2023-04-27 08:48:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import GooglePayButton from "@google-pay/button-react";
+const App = () => {
+  return (
+    <div className="App">
+      <GooglePayButton
+        environment="TEST"
+        paymentRequest={{
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          allowedPaymentMethods: [
+            {
+              type: "CARD",
+              parameters: {
+                allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                allowedCardNetworks: ["MASTERCARD", "VISA"],
+              },
+              tokenizationSpecification: {
+                type: "PAYMENT_GATEWAY",
+                parameters: {
+                  gateway: "example",
+                  gatewayMerchantId: "exampleGatewayMerchantId",
+                },
+              },
+            },
+          ],
+          merchantInfo: {
+            merchantId: " BCR2DN4TUTW2PMBR",
+            merchantName: "instamart",
+          },
+          transactionInfo: {
+            totalPriceStatus: "FINAL",
+            totalPriceLabel: "Total",
+            totalPrice: "100.00",
+            currencyCode: "USD",
+            countryCode: "US",
+          },
+        }}
+        onLoadPaymentData={(paymentRequest) => {
+          console.log("load payment data", paymentRequest);
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
